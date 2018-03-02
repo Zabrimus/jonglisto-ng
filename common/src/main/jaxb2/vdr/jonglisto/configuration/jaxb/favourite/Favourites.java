d0dae4d09f6a3fff9e3f0994da0878df9e0a0836
Zabrimus <github@hannebauer.org>
2018-03-02 21:04:27 +0100

    fixed build

--- common/src/main/jaxb2/vdr/jonglisto/configuration/jaxb/favourite/Favourites.java
+++ common/src/main/jaxb2/vdr/jonglisto/configuration/jaxb/favourite/Favourites.java
@@ -2,7 +2,7 @@
 // Diese Datei wurde mit der JavaTM Architecture for XML Binding(JAXB) Reference Implementation, v2.2.11 generiert 
 // Siehe <a href="http://java.sun.com/xml/jaxb">http://java.sun.com/xml/jaxb</a> 
 // Änderungen an dieser Datei gehen bei einer Neukompilierung des Quellschemas verloren. 
-// Generiert: 2018.02.27 um 05:49:05 PM CET 
+// Generiert: 2018.03.02 um 09:03:04 PM CET 
 //
 
 
